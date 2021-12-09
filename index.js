9da6c9baa02c4be020415b7ee431f7b3b95b8a7b
estDiegoG <est.diego.garnica@unimilitar.edu.co>
2021-12-09 11:59:49 -0500

    ajustes para deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extender:true}));
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
@@ -29,6 +29,6 @@ app.get('/',(req,res)=>{
 })
 
 //arrancar app
-app.listen(PORT, ()=>{
+app.listen(port,'0.0.0.0', ()=>{
     console.log('El servidor esta funcionando en el puerto '+ PORT)
 });
