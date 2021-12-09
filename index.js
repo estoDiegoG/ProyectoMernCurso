d0ffa99eeb1dda1461e2b5adadd7cc80e4143715
estDiegoG <est.diego.garnica@unimilitar.edu.co>
2021-12-09 12:18:02 -0500

    ajustes para deploy

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const app = express();
 conectarDB();
 
 //habilitar cors
-app.use(cors({credentials:true,origin: 'https://aqueous-garden-41189.herokuapp.com'}));
-app.options("*",cors());
+app.use(cors());
 
 //habilitar express json
 app.use(express.json({extender:true}));
 
 //puerto de la app
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //importar rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
@@ -30,6 +29,6 @@ app.get('/',(req,res)=>{
 })
 
 //arrancar app
-app.listen(port,'0.0.0.0', ()=>{
-    console.log('El servidor esta funcionando en el puerto ')
+app.listen(port, '0.0.0.0.',()=>{
+    console.log('El servidor esta funcionando en el puerto '+ PORT)
 });
